4ba42fbc26217d5f3ebac549d1f4bf78fe1182b6
HEMAPRIYARAVI <ravi.hemapriya@gmail.com>
2025-05-04 01:11:03 -0500

    Added HelloWorld.java – my first Java program 🎉

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Printing Hello World
+        System.out.println("Hello, Hema!");
+
+        // Declaring and printing different variable types
+        int age = 28;
+        double weight = 70;
+        char grade = 'A';
+        boolean isLearning = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Weight: " + weight);
+        System.out.println("Grade: " + grade);
+        System.out.println("Learning Java: " + isLearning);
+    }
+}
